fd5f36de62dbe64e136fc3caa7554f3478079211
Denne Kabaya <dudeholy35@gmail.com>
2025-02-01 12:31:58 -0800

    validationUtils

---
+++ DevArsenal/src/dataUtils/stringUtils.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//-------------trim(str)--------------------------//
+//---remove white space from both ends of a string
+
+function trim(){
+    
+}
---
+++ DevArsenal/src/dataUtils/validationUtils.js
@@ -0,0 +1,30 @@
+'use stict';
+///---------------------------------------data validation module---------------------------//
+
+//--------------General Data Validation---//
+//reusable validateData function
+
+//schema-based validation with Joi
+
+//custom error handling for structured responses
+
+
+
+//-------------custom Validation Rules---//
+
+//extend joi with custom validation methods
+
+
+//-------------Batch validation---//
+
+//validate multiple entries at once (eg, bulk API request)
+
+//return a summarry of valid and invalid items
+
+
+//-------------async validation pipeline---//
+
+
+
+
+//-------------file-Bsed Data validation---//
